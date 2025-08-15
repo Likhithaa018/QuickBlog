bf14da49c15f7c5168080c998cb37b8051614a40
Likhitha <likhithaa.187@example.com>
2025-08-15 15:54:17 +0530

    Initial commit - QuickBlog

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,8 +5,7 @@ const connectDB = async ()=>{
         mongoose.connection.on('connected', ()=> console.log //once db is connected, this will show up in terminal.
         ("Database Connected")
     )
-        await mongoose.connect(`${process.env.MONGODB_URI}/
-            quickblog`)                                        
+        await mongoose.connect(`${process.env.MONGODB_URI}/quickblog`)                                        
                                                              
     }catch (error){
     console.log(error.message);
